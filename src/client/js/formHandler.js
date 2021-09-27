03ad0323d2d63dee2366275fc89b706579a20062
a7med-hamdy <hamdiahmed906@gmail.com>
2021-09-28 00:17:49 +0200

    modifications

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,16 +7,24 @@ function handleSubmit(event) {
     const api = process.env.API
     const key = process.env.KEY
     const url = api + "key=" +key + '&txt=' + formText.value + '&lang=en';
+    if(formText.value === '')
+    {
+        alert("Empty input is invalid.")
+    }
+    else
+    {
+        fetch(url)
+        .then(res => res.json())
+        .then((res) => {
+            if(Client.checker(res))
+            {
+                Client.UpdateUI(res)
+            }
+        })
+        .catch(e => alert("lost connection"))
+    }
     formText.value = '';
-    fetch(url)
-    .then(res => res.json())
-    .then((res) => {
-        if(Client.checker(res))
-        {
-            Client.UpdateUI(res)
-        }
-    })
-    .catch(e => alert("lost connection"))
+    
 }
 
 export { handleSubmit }
